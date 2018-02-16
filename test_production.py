3ffd0a1a14d6fca10b3385715cc09305d89c99d8
Unknown <estelle.marti@gmail.com>
2018-02-16 14:42:14 +0100

    adapted test

--- test_production.py
+++ test_production.py
@@ -12,7 +12,7 @@ class TestProdution(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-class TestSquare(unittest.TestCase:
+class TestSquare(unittest.TestCase):
     def test_return_square(self):
         input = [0,1,2,3,4,5,6,7,8,9,10]
         output = [0,1,4,9,16,25,36,49,64,81,100]
