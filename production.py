87f575de769c249d48bb80bb1c82eff299db881e
Unknown <julie.rufino@student.unisg.ch>
2018-02-16 14:21:44 +0100

    production file changed with return_square

--- production.py
+++ production.py
@@ -3,12 +3,22 @@
 #For examples:
 #remove_duplicates([1,1,2,2])' should return '[1,2]'.
 
-a_list = [1,2,3,4,5,1,2,3]
+a_list1 = [1,2,3,4,5,1,2,3]
 
-def remove_duplicates(a_list):
+def remove_duplicates(a_list1):
+    new_list1 = []
+    for i in a_list1:
+        if i not in new_list1:
+            new_list1.append(i)
+    return new_list1
+print(remove_duplicates(a_list1))
+
+a_list = [0,1,2,3,4,5,6,7,8,9,10]
+
+def return_square(a_list):
     new_list = []
     for i in a_list:
-        if i not in new_list:
-            new_list.append(i)
+        new_list.append(i * i)
     return new_list
-print(remove_duplicates(a_list))
+
+print(return_square(a_list))
